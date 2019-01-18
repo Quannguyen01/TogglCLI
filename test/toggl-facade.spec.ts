7e727b2237d0e647f1db753d551435add73d5051
quannguyen01 <quannguyenhong.92@gmail.com>
2019-01-17 23:46:18 -0600

    Add test for stopping from facade

--- test/toggl-facade.spec.ts
+++ test/toggl-facade.spec.ts
@@ -8,8 +8,15 @@ describe('Toggl Facade test', () => {
     });
 
     it('should start a toggl entry and return a successful object', async () => {
-        let result = await (new TogglFacade()).start('Test adding new facade entry', 'Study');
+        let result = await (new TogglFacade()).start('Test adding new facade entry', 'Toggl CLI');
         expect(result.description).to.equal('Test adding new facade entry');
     });
 
+    it('should stop a toggl entry that is currently running', async() => {
+        const toggl = new TogglFacade();
+        await toggl.start('Test adding/stopping new entry from facade', 'Toggl CLI');
+
+        let result = await toggl.stop();
+        expect(result).to.be.true;
+    });
 });
