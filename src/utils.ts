d8274e3955910e9f00dcfd151124d006da729db0
quannguyen01 <quannguyenhong.92@gmail.com>
2019-01-19 11:41:15 -0600

    Linting and minor change from arrow function to function per mocha recommended

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,15 @@
 export function makePrettyTimeDuration(durationInSec: number) {
-    const MINUTE = 60,
-        HOUR = 60,
-        DAY = 24;
-    
-    let secDisplay, minDisplay, hourDisplay, dayDisplay: number;
+    const MINUTE = 60;
+    const HOUR = 60;
+    const DAY = 24;
 
-    secDisplay = durationInSec % MINUTE;
-    let minutes = Math.round(durationInSec / MINUTE);
-    
-    minDisplay = minutes % HOUR;
-    let hours = Math.round(minutes / HOUR);
+    const secDisplay = durationInSec % MINUTE;
+    const minutes = Math.round(durationInSec / MINUTE);
+    const minDisplay = minutes % HOUR;
+    const hours = Math.round(minutes / HOUR);
 
-    hourDisplay = hours % DAY;
-    dayDisplay = Math.round(hours / DAY);
+    const hourDisplay = hours % DAY;
+    const dayDisplay = Math.round(hours / DAY);
 
     return (dayDisplay > 0 ? `${dayDisplay} days - ` : '') +
         `${hourDisplay}:${minDisplay}:${secDisplay}`;
--- test/toggl-client.spec.ts
+++ test/toggl-client.spec.ts
@@ -2,23 +2,21 @@ import { TogglClientApi } from '../src/toggl-client';
 import { TimeEntry } from '../src/model/TimeEntry';
 import { startUp } from '../src/startup';
 import { expect } from 'chai';
-import { isRegExp } from 'util';
-import { TogglFacade } from '../src/toggl-facade';
 
 const PROJECT_TEST_ID = 148757817;
 const APP_NAME = 'my-toggl-client';
 
-describe('Toggl API Testing', () => {
+describe('Toggl API Testing', function() {
     let toggl: TogglClientApi;
-    before(() => {
+    before(function() {
         startUp();
     });
 
-    beforeEach(() => {
+    beforeEach(function() {
         toggl = new TogglClientApi();
     });
 
-    it('should create a time entry using toggl client api', async () => {
+    it('should create a time entry using toggl client api', async function() {
         let entry: TimeEntry = {
             description: 'Meeting with possible client',
             tags: ['billed'],
@@ -34,13 +32,13 @@ describe('Toggl API Testing', () => {
         expect(result.description).to.equal(entry.description);
     });
 
-    it('should find a toggl project with specified project name', async() => {
+    it('should find a toggl project with specified project name', async function() {
         let result = await toggl.findProjectId('Toggl CLI');
         expect(result).to.not.be.null;
         expect(result).to.be.not.undefined;
     });
 
-    it('should start a time entry', async() => {
+    it('should start a time entry', async function() {
         let entry: TimeEntry = {
             description: 'Testing starting time entry from Toggl client',
             tags: ['dev'],
@@ -55,7 +53,7 @@ describe('Toggl API Testing', () => {
             expect(result.description).to.equal(entry.description);
     });
 
-    it('should get the current running entry from toggl', async() => {
+    it('should get the current running entry from toggl', async function() {
         await toggl.startEntry({
             description: 'Testing adding new entry to get current',
             pid: PROJECT_TEST_ID,
@@ -70,7 +68,7 @@ describe('Toggl API Testing', () => {
         }
     });
 
-    it('should stop the current running entry in toggl', async() => {
+    it('should stop the current running entry in toggl', async function() {
         let runningEntry = await toggl.startEntry({
             description: 'Testing adding new entry to stop',
             pid: PROJECT_TEST_ID,
